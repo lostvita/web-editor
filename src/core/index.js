8a8d5aaab32429d5c7b04372552132f394976f14
安歌 <qdwgdut@126.com>
2021-03-06 23:41:24 +0800

    feat(editor): v1.0.0

--- src/core/index.js
+++ src/core/index.js
@@ -8,14 +8,18 @@ class Editor {
   constructor(options) {
     this.__init(options)
   }
+
   __init(options) {
     this.$options = Object.assign({}, options)
   }
+
   mount(ele) {
     const container = typeof ele === 'string' ? document.querySelector(ele) : ele
     this.$options.el = container
     const ed = this
     initEditor(ed)
   }
+
+  clear() {}
 }
 export default Editor
